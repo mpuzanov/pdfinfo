23f6b86a88e0b2d48e34d96ad7d5380b0b0a6834
mpuzanov@mail.ru <mpuzanov>
2020-02-13 11:43:25 +0400

    Добавил тест

---
+++ pkg/infopdf/infopdf_test.go
@@ -0,0 +1,18 @@
+package infopdf_test
+
+import (
+	pi "pdfinfo/pkg/infopdf"
+	"testing"
+)
+
+func TestGetPageCountPdf(t *testing.T) {
+	fileName := "./../../example/test_file.pdf"
+	want := 2
+	got, err := pi.GetPageCountPdf(fileName)
+	if err != nil {
+		t.Errorf("got=%v, expected=%v, error: %v", got, want, err)
+	}
+	if got != want {
+		t.Errorf("got=%v, expected=%v", got, want)
+	}
+}
